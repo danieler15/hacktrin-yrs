c0a2168a2c28af174f5825f24feaac95fc21c240
Virginia <virg.cook@earthlink.net>
2013-06-29 14:09:04 -0400

    Edit of places api stuff

--- HackTrin/parseFoodPlaces.php
+++ HackTrin/parseFoodPlaces.php
@@ -1,20 +1,21 @@
 <?php
-$name;
+$name="chipotle";
 
 $zipcode;
 $location;
 $status;
-$url="https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyDxQyIwXug-O2Ha9hVRWIL3kuSdOY7PPD4&query="+$name+"+new+york&sensor=true&types=restaurant";
+$url="https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyDxQyIwXug-O2Ha9hVRWIL3kuSdOY7PPD4&query=".'$name'."+new+york&sensor=true&types=restaurant";
     $json = file_get_contents($url);
- //   echo $json;
+    echo $json;
     $data = json_decode($json, TRUE);
     $status=$data["status"];
     echo $status;
-    if($status="OK"){
+    /*if($status=="OK"){
     	getInfo($data)
     }
     function getInfo($data){
-    }
+    	echo $json;
+    }*/
     
 //	echo $data;
 //	var_dump($data);
